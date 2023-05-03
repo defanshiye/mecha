c919681f1882de8f93a11f7bedb4b0d0817141ab
kickr <hehegaoxiang@163.com>
2023-05-03 19:23:15 +0800

    feature: add express

---
+++ scripts/dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// const chalk = require("chalk");
+// const server = require("./server");
+// const clearConsole = require("./utils/clearConsole");
+
+// const port = parseInt(process.env.PORT, 10) || 3000;
+// const HOST = process.env.HOST || "0.0.0.0";
+
+// server.listen(port, () => {
+//   clearConsole();
+//   chalk.cyna("NIHoS");
+//   // console.log(`Example app listening on port ${port}`);
+// });
+
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello world!"));
