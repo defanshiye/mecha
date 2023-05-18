cda878c0d5bf7b2c9613870d78efec702bad8983
KICKR <hehegaoxiang@163.com>
2023-05-18 15:21:28 +0800

    feature: project structure

---
+++ src/client/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Hello SSR</h1>
+    <button onClick={() => alert('hello')}>Click Me</button>
+  </div>
+);
+
+export default App;
