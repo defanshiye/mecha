80598efb985eba7bcc5eee23f97c7660446acf88
KICKR <hehegaoxiang@163.com>
2023-05-18 18:31:22 +0800

    feature: SSR complete by @babel/register

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,56 +1,5 @@
 'use strict';
 
-const express = require('express');
-const webpack = require('webpack');
-const isObject = require('is-object');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const clientConfig = require('../config/client.config');
+require('@babel/register');
 
-const compiler = webpack(clientConfig);
-const app = express();
-
-function normalizeAssets(assets) {
-  if (isObject(assets)) {
-    return Object.values(assets);
-  }
-
-  return Array.isArray(assets) ? assets : [assets];
-}
-
-app.use(express.static('public'));
-app.use(webpackDevMiddleware(compiler, { serverSideRender: true }));
-app.use(webpackHotMiddleware(compiler));
-app.use((req, res) => {
-  const { devMiddleware } = res.locals.webpack;
-  const outputFileSystem = devMiddleware.outputFileSystem;
-  const jsonWebpackStats = devMiddleware.stats.toJson();
-  const { assetsByChunkName, outputPath } = jsonWebpackStats;
-
-  // Then use `assetsByChunkName` for server-side rendering
-  // For example, if you have only one main chunk:
-  res.send(`
-<html>
-  <head>
-    <title>My App</title>
-    <style>
-    ${normalizeAssets(assetsByChunkName.main)
-      .filter(path => path.endsWith('.css'))
-      .map(path => outputFileSystem.readFileSync(path.join(outputPath, path)))
-      .join('\n')}
-    </style>
-  </head>
-  <body>
-    <div id="app"></div>
-    ${normalizeAssets(assetsByChunkName.main)
-      .filter(path => path.endsWith('.js'))
-      .map(path => `<script src="${path}"></script>`)
-      .join('\n')}
-  </body>
-</html>
-  `);
-});
-
-app.listen(3000, () => {
-  console.log('app is running: ...');
-});
+require('../src/server/server');
--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.getElementById('app'));
-
-root.render(<App />);
+hydrateRoot(document.getElementById('app'), <App />);
